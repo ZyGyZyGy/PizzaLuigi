590a1154e05fdb234a6e387aadd60c397657de15
Alex <jegorov.aleksandr@gmail.com>
2017-01-04 14:54:42 +0100

    HS 18 - GET REQUEST VERWERKEN - Request parameters in de query string

--- PizzaLuigi/src/be/vdab/servlets/PizzaDetailServlet.java
+++ PizzaLuigi/src/be/vdab/servlets/PizzaDetailServlet.java
@@ -1,8 +1,6 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,23 +9,21 @@ import javax.servlet.http.HttpServletResponse;
 
 import be.vdab.repositories.PizzaRepository;
 
-@WebServlet("/pizzas.htm")
-public class PizzaServlet extends HttpServlet {
+@WebServlet("/pizzas/detail.htm")
+public class PizzaDetailServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    private static final String VIEW = "/WEB-INF/JSP/pizzas.jsp";
-    private static final String PIZZAS_REQUESTS = "pizzasRequests";
+    private static final String VIEW = "/WEB-INF/JSP/pizzadetail.jsp";
     private final PizzaRepository pizzaRepository = new PizzaRepository();
-
-    @Override
-    public void init() throws ServletException {
-	this.getServletContext().setAttribute(PIZZAS_REQUESTS, new AtomicInteger());
-    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	    throws ServletException, IOException {
-	((AtomicInteger) this.getServletContext().getAttribute(PIZZAS_REQUESTS)).incrementAndGet();
-	request.setAttribute("pizzas", pizzaRepository.findAll());
+	try {
+	    pizzaRepository.read(Long.parseLong(request.getParameter("id")))
+		    .ifPresent(pizza -> request.setAttribute("pizza", pizza));
+	} catch (NullPointerException | NumberFormatException ex) {
+	    request.setAttribute("fout", "id niet correct");
+	}
 	request.getRequestDispatcher(VIEW).forward(request, response);
     }
 
